5461823a06eacc59b4624974512bb8c29b40d95d
Max <somethingfunny14@gmail.com>
2015-05-03 10:59:20 -0400

    Added main menu, game launcher, and data store update screens

---
+++ Trival Pursuif/Trival Pursuif/src/game/Game.java
@@ -0,0 +1,53 @@
+package game;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AppState;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.math.ColorRGBA;
+import com.jme3.niftygui.*;
+
+import de.lessvoid.nifty.Nifty;
+
+public class Game extends SimpleApplication {
+
+	@Override
+	public void simpleInitApp() {
+		/*Box b = new Box(1, 1, 1);
+		Geometry geom = new Geometry("Box", b);
+		Material mat = new Material(assetManager,
+				"Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", ColorRGBA.Blue);
+		geom.setMaterial(mat);
+		rootNode.attachChild(geom);*/
+
+		NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(
+				assetManager, inputManager, audioRenderer, guiViewPort);
+		Nifty nifty = niftyDisplay.getNifty();
+		
+		// Create screen controllers
+		StartScreenState startScreenState = new StartScreenState(this, nifty);
+		LauncherScreenState launcherScreenState = new LauncherScreenState(this, nifty);
+		UpdateQuestionVaultScreenState updateQuestionVaultScreenState = new UpdateQuestionVaultScreenState(this, nifty);
+		
+		// Register with app's stateManager
+		stateManager.attach(startScreenState);
+		stateManager.attach(launcherScreenState);
+		stateManager.attach(updateQuestionVaultScreenState);
+		
+		// notify boilerplate
+		guiViewPort.addProcessor(niftyDisplay);
+		flyCam.setDragToRotate(true);
+		
+		// load XML GUI into nifty
+		nifty.fromXml("Interface/screens.xml", "start", startScreenState, launcherScreenState, updateQuestionVaultScreenState);
+		inputManager.setCursorVisible(true);
+	}
+
+	public static void main(String[] args) {
+		Game app = new Game();
+		app.start();
+	}
+}
---
+++ Trival Pursuif/Trival Pursuif/src/game/StartScreenState.java
@@ -0,0 +1,78 @@
+package game;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.math.ColorRGBA;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+public class StartScreenState extends AbstractAppState implements ScreenController {
+	private Nifty nifty;
+	private SimpleApplication app;
+
+	// Needed for Nifty to work
+	public StartScreenState() {
+	}
+	
+	public StartScreenState(SimpleApplication app, Nifty nifty) {
+		this.app = app;
+		this.nifty = nifty;
+	}
+
+	public void switchScreens(String nextScreen) {
+		nifty.gotoScreen(nextScreen);
+	}
+
+	public void quitGame() {
+		// TODO: This doesn't work right now.
+		app.stop();
+	}
+
+	@Override
+	public void initialize(AppStateManager stateManager, Application app) {
+		// TODO: initialize your AppState, e.g. attach spatials to rootNode
+		// this is called on the OpenGL thread after the AppState has been attached
+		super.initialize(stateManager, app);
+		
+		/** init the screen */
+	}
+	
+	@Override
+	public void update(float tpf) {
+		/** any main loop action happens here */
+	}
+	
+	@Override
+	public void cleanup() {
+		// TODO: clean up what you initialized in the initialize method,
+		// e.g. remove all spatials from rootNode
+		// this is called on the OpenGL thread after the AppState has been detached
+		super.cleanup();
+	}
+
+	@Override
+	public void bind(Nifty arg0, Screen arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEndScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStartScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
